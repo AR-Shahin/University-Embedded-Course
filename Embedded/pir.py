b9c4a254d01c3a84338c32a560d4bbf3c85b63fb
ar-shahin <mdshahinmije96@gmail.com>
2023-01-02 17:53:51 +0600

    pir sensor

---
+++ Embedded/pir.py
@@ -0,0 +1,9 @@
+# Link : https://projects.raspberrypi.org/en/projects/physical-computing/11
+from gpiozero import MotionSensor
+
+pir = MotionSensor(4)
+
+while True:
+	pir.wait_for_motion()
+	print("You moved")
+	pir.wait_for_no_motion()
